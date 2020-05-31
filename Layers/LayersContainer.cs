15e86a3695c8be16bc6a33e415358babebff3b94
Matan Weissbuch <matan2050@gmail.com>
2020-05-31 17:21:32 +0300

    initial implementation of layer container

---
+++ Layers/LayersContainer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace MIVAT.Layers
+{
+    public class LayersContainer
+    {
+        public Dictionary<string, Layer> Container { get; private set; }
+
+        public void AddLayerToContainer(Layer _layer)
+        {
+            if (!Container.TryGetValue(_layer.Name, out Layer layer))
+            {
+                Container.Add(layer.Name, layer);
+            }
+            else
+            {
+                throw new ArgumentException("layer already exists");
+            }
+        }
+
+        public void RemoveLayerFromContainer(string name)
+        {
+            if (Container.TryGetValue(name, out Layer layer))
+            {
+                Container.Remove(name);
+            }
+            else
+            {
+                throw new ArgumentException("layer to remove does not exist");
+            }
+        }
+
+        public List<string> GetContainerStatus()
+        {
+            List<string> layersName = new List<string>(Container.Count);
+            foreach (var layerName in Container.Keys)
+            {
+                layersName.Add(layerName);
+            }
+            return layersName;
+        }
+    }
+}
